733072b206f79a23ab170c4acc5d220cf5c516c7
wangkun <kun64855@gmail.com>
2025-02-24 15:57:47 +0800

    首页布局完成

--- pages/sys/home/index.vue
+++ pages/sys/home/index.vue
@@ -1,46 +1,81 @@
 <template>
-	<view class="wrap">
-		<js-lang title="home.title" :showBtn="true"></js-lang>
-		<u-swiper :height="300" :list="imgList" :title="false" @click="imgListClick"></u-swiper>
-		<view class="toolbar u-m-b-20">
-			<u-grid class="grid" :col="3" :border="false">
-				<u-grid-item :index="0" @click="navTo('/pages/bpm/myTaskTodo')">
-					<u-badge :count="todoCount"></u-badge>
-					<u-icon class="grid-icon" name="clock" :size="80" :style="{ color: '#ea9a44' }"></u-icon>
-					<view class="grid-text">待办任务</view>
-				</u-grid-item>
-				<u-grid-item :index="1" @click="navTo('/pages/bpm/myTaskHistory')">
-					<u-icon class="grid-icon" name="checkmark-circle" :size="80" :style="{ color: '#47cb66' }"></u-icon>
-					<view class="grid-text">已办任务</view>
-				</u-grid-item>
-				<u-grid-item :index="2" @click="navTo('/pages/bpm/myRuntime')">
-					<u-icon class="grid-icon" name="order" :size="80" :style="{ color: '#5a98ea' }"></u-icon>
-					<view class="grid-text">我相关的</view>
-				</u-grid-item>
-			</u-grid>
-		</view>
-		<u-collapse class="box u-p-b-5" :accordion="false" :arrow="true">
-			<view class="item" v-for="(menu, index) in menuList" :key="menu.menuCode">
-				<u-collapse-item :open="true">
-					<view class="title" slot="title">
-						<u-icon :name="menu.menuIcon != '' ? menu.menuIcon : 'home'" :size="40"
-							:style="{ color: menu.menuColor != '' ? menu.menuColor : '#666' }"></u-icon>
-						<view class="text" :style="{ color: menu.menuColor != '' ? menu.menuColor : '#666' }"
-							>{{menu.menuName}}</view>
-					</view>
-					<u-grid class="grid u-m-t-20" :col="3" :border="false">
-						<u-grid-item v-for="(child, index2) in menu.childList" :key="child.menuCode" @click="navTo(child.url)">
-							<u-icon class="grid-icon" :name="child.menuIcon != '' ? child.menuIcon : 'order'" :size="80"
-								:style="{ color: child.menuColor != '' ? child.menuColor : '#666' }"></u-icon>
-							<view class="grid-text" :style="{ color: child.menuColor != '' ? child.menuColor : '#666' }"
-								>{{child.menuName}}</view>
-						</u-grid-item>
-					</u-grid>
-				</u-collapse-item>
+	<view class="container">
+		<!-- 顶部区域 -->
+		<view class="header">
+			<view class="avatar">
+				<image src="/static/images/avatar.png" ></image>
 			</view>
-		</u-collapse>
+			<view class="date-display">
+				<text class="date-text">2023-12-01</text>
+			</view>
+
+			
+			<view class="date-picker" @click="handleSkip">
+				<u-icon 
+					name="calendar" 
+					size="40" 
+					color="#ffffff"
+					customStyle="background-color: #42d392; padding: 20rpx; border-radius: 50%;"
+				></u-icon>
+			</view>
+
+		</view>
+
+		<!-- 主要内容区 -->
+		<view class="content">
+			<view class="text-content">
+				<view class="hello">
+					您好, KOTO
+				</view>
+				<view class="weight-tip">
+					你昨天增加了2kg, 继续保持!
+				</view>
+			</view>
+
+			<view class="progress-circle">
+				<view class="circle-wrapper">
+					<!-- 这里使用自定义组件或canvas绘制半圆进度 -->
+					<u-circle-progress
+						:percentage="60"
+						:width="200"
+						activeColor="#42d392"
+						inactiveColor="#eee"
+					></u-circle-progress>
+					<view class="circle-content">
+						<u-icon name="fire" size="24" color="#ff9500"></u-icon>
+						<text class="unit">大卡</text>
+						<text class="value">1739</text>
+						<text class="total">/ 2925 kcal</text>
+					</view>
+				</view>
+			</view>
+
+			<view class="nutrition-stats">
+				<view class="stat-item left">
+					<view class="number">134<text class="unit">g</text></view>
+					<view class="label">
+						<text>总碳水化合物</text>
+						<text class="percentage">28%</text>
+					</view>
+					<view class="progress-bar">
+						<view class="progress" style="width: 28%;"></view>
+					</view>
+				</view>
+				<view class="stat-item right">
+					<view class="number">94<text class="unit">g</text></view>
+					<view class="label">
+						<text>总脂肪</text>
+						<text class="percentage">81%</text>
+					</view>
+					<view class="progress-bar">
+						<view class="progress" style="width: 81%;"></view>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
+
 <script>
 /**
  * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
@@ -49,180 +84,190 @@ export default {
 	data() {
 		return {
 			
-			imgList: [
-				{image: '/static/jeesite/banner/1.svg'},
-				{image: '/static/jeesite/banner/2.svg'},
-				{image: '/static/jeesite/banner/3.svg'}
-			],
-			
-			todoCount: 0,
-			
-			menuList: [
-				{
-					menuCode: 'a-1',
-					menuName: '增删改查',
-					menuIcon: 'file-text',
-					menuColor: '',
-					url: '',
-					childList: [
-						{
-							menuCode: 'a13',
-							menuName: '列表',
-							menuIcon: 'thumb-up',
-							menuColor: '',
-							url: '/pages/testData/list',
-						},
-						{
-							menuCode: 'a11',
-							menuName: '新增',
-							menuIcon: 'plus-circle',
-							menuColor: '',
-							url: '/pages/testData/form',
-						},
-						{
-							menuCode: 'a10',
-							menuName: '请假',
-							menuIcon: 'calendar',
-							menuColor: '',
-							url: '/pages/oa/oaLeave/list',
-						},
-					]
-				},
-				{
-					menuCode: 'a',
-					menuName: '公文管理',
-					menuIcon: 'home',
-					menuColor: '#919328',
-					url: '',
-					childList: [
-						{
-							menuCode: 'a1',
-							menuName: '收文',
-							menuIcon: 'email',
-							menuColor: '#919328',
-							url: '/pages/testData/form',
-						},
-						{
-							menuCode: 'a2',
-							menuName: '发文',
-							menuIcon: 'bookmark',
-							menuColor: '#919328',
-							url: '/pages/testData/form',
-						},
-						{
-							menuCode: 'a3',
-							menuName: '查询',
-							menuIcon: 'search',
-							menuColor: '#919328',
-							url: '/pages/testData/list',
-						}
-					]
-				},
-				{
-					menuCode: 'a-2',
-					menuName: '功能列表',
-					menuIcon: '',
-					menuColor: '#0d9311',
-					url: '',
-					childList: [
-						{
-							menuCode: 'a21',
-							menuName: '找回密码',
-							menuIcon: '',
-							menuColor: '#0d9311',
-							url: '/pages/sys/login/forget',
-						},
-						{
-							menuCode: 'a22',
-							menuName: '注册用户',
-							menuIcon: '',
-							menuColor: '#0d9311',
-							url: '/pages/sys/login/reg',
-						},
-						{
-							menuCode: 'a23',
-							menuName: '个人资料',
-							menuIcon: '',
-							menuColor: '#0d9311',
-							url: '/pages/sys/user/info',
-						},{
-							menuCode: 'a24',
-							menuName: '关于我们',
-							menuIcon: '',
-							menuColor: '#0d9311',
-							url: '/pages/sys/user/about',
-						},
-						{
-							menuCode: 'a25',
-							menuName: '修改密码',
-							menuIcon: '',
-							menuColor: '#0d9311',
-							url: '/pages/sys/user/pwd',
-						},
-						{
-							menuCode: 'a26',
-							menuName: '意见反馈',
-							menuIcon: '',
-							menuColor: '#0d9311',
-							url: '/pages/sys/user/comment',
-						},
-						{
-							menuCode: 'a27',
-							menuName: '系统设置',
-							menuIcon: '',
-							menuColor: '#0d9311',
-							url: '/pages/sys/user/setting',
-						},
-						{
-							menuCode: 'a28',
-							menuName: '列表演示',
-							menuIcon: '',
-							menuColor: '#0d9311',
-							url: '/pages/testData/list',
-						},
-						{
-							menuCode: 'a29',
-							menuName: '表单演示',
-							menuIcon: '',
-							menuColor: '#0d9311',
-							url: '/pages/testData/form',
-						}
-					]
-				},
-			],
-				
-		};
-	},
-	onLoad() {
-		//this.refreshCount();
-	},
-	onShow() {
-		this.refreshCount();
+		}
 	},
 	methods: {
-		navTo(url) {
-			uni.navigateTo({
-				url: url
-			});
-		},
-		refreshCount() {
-			// 获取待办个数
-			this.$u.api.bpm.myTaskList({status:1,pageSize:1}).then(res => {
-				this.todoCount = res.count || 0;
-			});
-		},
-		imgListClick(index) {
-			console.log(`点击了第${index + 1}页图片`)
-		},
-		itemClick(index) {
-			console.log(index);
+		handleSkip() {
+			// 处理日期选择
 		}
 	}
 };
 </script>
+
 <style lang="scss">
 @import 'index.scss';
 page {
 	background-color: #f8f8f8;
+}
+
+.container {
+	padding: 40rpx;
+	background-color: #f8f8f8;
+}
+
+.header {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	margin-bottom: 40rpx;
+	padding: 20rpx;
+	
+	.avatar {
+		width: 120rpx;
+		height: 120rpx;
+		border-radius: 50%;
+		overflow: hidden;
+		margin-right: 20rpx;
+		
+		image {
+			width: 120rpx;
+			height: 120rpx;
+			border-radius: 50%;
+		}
+	}
+	
+	.date-display {
+		flex: 1;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		
+		.date-text {
+			font-family: "Microsoft YaHei", sans-serif;
+			font-size: 32rpx;
+			font-weight: bold;
+			color: #333;
+		}
+	}
+	
+	.date-picker {
+		width: 120rpx;
+		height: 120rpx;
+		border-radius: 50%;
+		background: #42d392;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		box-shadow: 0 4rpx 12rpx rgba(66, 211, 146, 0.3);
+		
+		.u-icon {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+	}
+}
+
+.content {
+	background: #fff;
+	border-radius: 20rpx;
+	padding: 30rpx;
+	
+	.text-content {
+		margin-bottom: 40rpx;
+		
+		.hello {
+			font-size: 40rpx;
+			font-weight: bold;
+			color: #333;
+			margin-bottom: 10rpx;
+		}
+		
+		.weight-tip {
+			font-size: 28rpx;
+			color: #666;
+		}
+	}
+	
+	.progress-circle {
+		display: flex;
+		justify-content: center;
+		margin: 60rpx 0;
+		
+		.circle-wrapper {
+			position: relative;
+			
+			.circle-content {
+				position: absolute;
+				top: 50%;
+				left: 50%;
+				transform: translate(-50%, -50%);
+				text-align: center;
+				
+				.unit {
+					font-size: 24rpx;
+					color: #666;
+					display: block;
+				}
+				
+				.value {
+					font-size: 48rpx;
+					color: #333;
+					font-weight: bold;
+					margin: 10rpx 0;
+				}
+				
+				.total {
+					font-size: 24rpx;
+					color: #999;
+				}
+			}
+		}
+	}
+	
+	.nutrition-stats {
+		display: flex;
+		justify-content: space-between;
+		margin-top: 40rpx;
+		
+		.stat-item {
+			flex: 1;
+			padding: 0 20rpx;
+			
+			.number {
+				font-size: 40rpx;
+				font-weight: bold;
+				color: #333;
+				margin-bottom: 10rpx;
+				
+				.unit {
+					font-size: 24rpx;
+					margin-left: 4rpx;
+				}
+			}
+			
+			.label {
+				display: flex;
+				justify-content: space-between;
+				font-size: 24rpx;
+				color: #666;
+				margin-bottom: 10rpx;
+				
+				.percentage {
+					color: #42d392;
+				}
+			}
+			
+			.progress-bar {
+				height: 6rpx;
+				background: #eee;
+				border-radius: 3rpx;
+				
+				.progress {
+					height: 100%;
+					background: #42d392;
+					border-radius: 3rpx;
+				}
+			}
+			
+			&.left .progress {
+				background: #42d392;
+			}
+			
+			&.right .progress {
+				background: #ff9500;
+			}
+		}
+	}
 }
 </style>
