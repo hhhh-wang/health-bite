90717c43e8d2b0bcc987817575f570ac309970a9
wangkun <kun64855@gmail.com>
2025-03-01 22:34:18 +0800

    修正首页

--- pages/sys/home/index.vue
+++ pages/sys/home/index.vue
@@ -47,25 +47,38 @@
 				</view>
 			</view>
 		</view>
-		<view class="nutrition-stats">
-			<view class="stat-item left">
-				<view class="number">134<text class="unit">g</text></view>
-				<view class="label">
-					<text>总碳水化合物</text>
-					<text class="percentage">28%</text>
+		<view class="nutrition-cards">
+			<!-- 碳水化合物卡片 -->
+			<view class="nutrition-card">
+				<view class="card-top">
+					<view class="nutrient-title">总碳水化合物</view>
+					<view class="nutrient-value">
+						<text class="number">134</text>
+						<text class="unit">g</text>
+					</view>
 				</view>
-				<view class="progress-bar">
-					<view class="progress" style="width: 28%;"></view>
+				<view class="progress-info">
+					<text class="percentage">28%</text>
+					<view class="progress-bar">
+						<view class="progress carbs" style="width: 28%;"></view>
+					</view>
 				</view>
 			</view>
-			<view class="stat-item right">
-				<view class="number">94<text class="unit">g</text></view>
-				<view class="label">
-					<text>总脂肪</text>
-					<text class="percentage">81%</text>
+			
+			<!-- 脂肪卡片 -->
+			<view class="nutrition-card">
+				<view class="card-top">
+					<view class="nutrient-title">总脂肪</view>
+					<view class="nutrient-value">
+						<text class="number">94</text>
+						<text class="unit">g</text>
+					</view>
 				</view>
-				<view class="progress-bar">
-					<view class="progress" style="width: 81%;"></view>
+				<view class="progress-info">
+					<text class="percentage">81%</text>
+					<view class="progress-bar">
+						<view class="progress fat" style="width: 81%;"></view>
+					</view>
 				</view>
 			</view>
 		</view>
@@ -415,13 +428,13 @@ page {
 
 .content {
 	background: transparent;
-	border-radius: 20rpx;
+	border-radius: 50rpx;
 	padding: 0 20rpx;
 }
 
 .simple-card {
 	background-color: rgb(235, 246, 214);
-	border-radius: 30rpx;
+	border-radius: 50rpx;
 	padding: 40rpx 30rpx;
 	margin-bottom: 40rpx;
 	box-shadow: 0 4rpx 12rpx rgba(0, 0, 0, 0.05);
@@ -469,13 +482,13 @@ page {
 		.progress-bar {
 			height: 16rpx;
 			background-color: rgba(255, 255, 255, 0.6);
-			border-radius: 8rpx;
+			border-radius: 50rpx;
 			overflow: hidden;
 			
 			.progress-fill {
 				height: 100%;
 				background-color: #42d392;
-				border-radius: 8rpx;
+				border-radius: 50rpx;
 				transition: width 0.3s ease-in-out;
 			}
 		}
@@ -491,66 +504,81 @@ page {
 		text {
 			background-color: rgba(255, 255, 255, 0.6);
 			padding: 8rpx 16rpx;
-			border-radius: 20rpx;
+			border-radius: 50rpx;
 		}
 	}
 }
 
-.nutrition-stats {
+.nutrition-cards {
+	margin: 30rpx 20rpx;
 	display: flex;
-	justify-content: space-between;
-	margin-top: 40rpx;
-	background-color: rgb(240, 241, 241);
-	padding: 30rpx;
-	border-radius: 50rpx;
+	flex-direction: row;
+	gap: 20rpx;
 	
-	.stat-item {
+	.nutrition-card {
 		flex: 1;
-		padding: 0 20rpx;
+		background-color: rgb(240, 241, 241);
+		border-radius: 50rpx;
+		padding: 24rpx;
+		box-shadow: 0 2rpx 10rpx rgba(0, 0, 0, 0.05);
 		
-		.number {
-			font-size: 50rpx;
-			font-weight: bold;
-			color: #333;
-			margin-bottom: 20rpx;
-			
-			.unit {
-				font-size: 30rpx;
-				margin-left: 4rpx;
-			}
-		}
-		
-		.label {
+		.card-top {
 			display: flex;
-			justify-content: space-between;
-			font-size: 30rpx;
-			color: #666;
-			margin-bottom: 30rpx;
+			flex-direction: column;
+			margin-bottom: 16rpx;
 			
+			.nutrient-title {
+				font-size: 26rpx;
+				color: #666;
+				margin-bottom: 8rpx;
+			}
+			
+			.nutrient-value {
+				display: flex;
+				align-items: baseline;
+				
+				.number {
+					font-size: 44rpx;
+					font-weight: bold;
+					color: #333;
+				}
+				
+				.unit {
+					font-size: 26rpx;
+					color: #999;
+					margin-left: 4rpx;
+				}
+			}
+		}
+		
+		.progress-info {
 			.percentage {
+				font-size: 30rpx;
 				color: #42d392;
+				margin-bottom: 20rpx;
+				display: block;
+				text-align: right;
 			}
-		}
-		
-		.progress-bar {
-			height: 6rpx;
-			background: #eee;
-			border-radius: 50rpx;
-			margin-bottom: 30rpx;
 			
-			.progress {
-				height: 100%;
-				background: #42d392;
+			.progress-bar {
+				height: 8rpx;
+				background-color: #eee;
 				border-radius: 50rpx;
+				overflow: hidden;
+				
+				.progress {
+					height: 100%;
+					border-radius: 50rpx;
+					
+					&.carbs {
+						background-color: #ffaa00;
+					}
+					
+					&.fat {
+						background-color: #ff6b6b;
+					}
+				}
 			}
-		}
-		
-		&.left .progress {
-			background: #ff9500;
-		}
-		
-		&.right .progress {
-			background: #ff9500;
 		}
 	}
 }
